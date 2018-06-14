84f19a33e5d56f6911d7f58933d16715e9706811
Jim Shaddix <jimmy.shaddix2.0@gmail.com>
2018-06-14 13:38:14 -0600

    Added Lorentzian Derivation
    derive the sepperate components of the lorentz function.

--- docs/3-Analyses/matlab-analyses/jim-analyses/CSV_Data/main.m
+++ docs/3-Analyses/matlab-analyses/jim-analyses/CSV_Data/main.m
@@ -3,3 +3,4 @@ path_to_csv_files = ['~/Documents/Pro/Git/Repos/Lab-Work/docs/'...
                       '2-Files_To_Analyze/31 May 18 TaV2/'];  
                   
 csv_data = RUSCSVload(path_to_csv_files);
+
