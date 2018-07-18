5a98b4b558afbf3d88fa1bacd8ae263c36d47c24
cepheid42 <39919895+cepheid42@users.noreply.github.com>
2018-07-17 18:25:41 -0600

    idk

--- docs/3-Analysis/tdms_peak_analysis/Andrew_main.m
+++ docs/3-Analysis/tdms_peak_analysis/Andrew_main.m
@@ -3,6 +3,8 @@ file1 = 'C:\Users\Cepheid\Documents\GitHub\rus-probe\docs\2-Files_To_Analyze\31 
 % file2 = 
 tdms_data = Store_TDMS_Data(file1);
 
+peak_width = 0.01*10^6;
+
 %% Fitting stuff
 perform_fit = true;
 if perform_fit == true
@@ -17,7 +19,7 @@ if perform_fit == true
                     {tdms_data.signal_x},  ...
                     {tdms_data.signal_y},  ...
                     {tdms_data.(peaks)},   ...
-                     peak_width);
+                    peak_width);
 
     % SET: fit_data
     for i = 1:length(fit_data)
@@ -31,16 +33,38 @@ if perform_fit == true
     
 end
 
-%% Plot shit
-i = 7;
-x = tdms_data(i).signal_x;
-y = tdms_data(i).signal_y;
-freq = tdms_data(i).frequency./1000;
+%% Plot peaks
+hold on;
+
+for num = 1:5:41
+    x = tdms_data(num).signal_x;
+    y = tdms_data(num).signal_y;
+    amp = sqrt(x.^2 + y.^2);
+    freq = tdms_data(num).frequency./1000;
+
+    plot(freq, amp)
+
+    name = tdms_data(num).file_name(14:length(tdms_data(num).file_name)-5);
+    l = legend(name);
+    set(l,'Interpreter', 'none');
+end
+%% Plot gamma
+temp = [tdms_data.temperature];
+gamma = [];
+peak = [1,1,1,2,3,1,2,2,
+% for ii = 1:length(tdms_data)
+%     for jj = 1:length(tdms_data(ii).mag_given_peaks)
+%    
+%     end
+%     gamma(end+1) = tdms_data(ii).mag_given_peaks(1).fit.gamma;
+% end
 
 hold on;
-plot(freq, x)
-plot(freq, y)
+plot(temp, gamma, 'bo')
 
 name = tdms_data(i).file_name(14:length(tdms_data(i).file_name)-5);
 l = legend(name);
 set(l,'Interpreter', 'none');
+
+%% Gamma Shite
+
